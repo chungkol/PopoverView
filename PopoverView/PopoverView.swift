5520bbf3872dcb9f70ec3ce3b3c4bd800a05fafc
chung <chungvtgc00989@fpt.edu.vn>
2016-08-24 11:06:04 +0700

    update

--- PopoverView/PopoverView.swift
+++ PopoverView/PopoverView.swift
@@ -16,20 +16,26 @@ class PopoverView: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+   
+    @IBAction func actionbutton(sender: AnyObject) {
+        if (sender.tag == 101) {
+            print("Like")
+        }
+        if (sender.tag == 102) {
+            print("Love")
+        }
+        if (sender.tag == 103) {
+            print("Wow")
+        }
+        if (sender.tag == 104) {
+            print("Sad")
+        }
+        if (sender.tag == 105) {
+            print("angry")
+        }
+        if (sender.tag == 106) {
+            print("Like")
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
